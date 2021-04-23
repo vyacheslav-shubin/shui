22379825a2a5a8e018b3f7cd2e75ce8676b35671
Vyacheslav Shubin <shubin-vv@krista.ru>
2021-04-23 20:18:55 +0300

    motion override

--- firmware/sapphire_pro/Coniguration.h
+++ firmware/sapphire_pro/Coniguration.h
@@ -1047,7 +1047,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 10, 10, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 0, -38, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
